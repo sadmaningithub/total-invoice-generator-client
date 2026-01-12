5c31366eb3943e56333a4dc25b1be2f0fe101032
sadmaningithub <sadmanahmed261@gmail.com>
2026-01-12 23:09:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <div>
+      <button className="btn">Default</button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/InvoiceForm.jsx
@@ -0,0 +1,7 @@
+
+
+export default function InvoiceForm() {
+  return (
+    <div>InvoiceForm</div>
+  )
+}
